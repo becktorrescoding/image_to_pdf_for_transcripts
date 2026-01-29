d997c3d68ba85fd4008105027627fc27e5bae1e7
becktorrestatwit <becktorrest@wit.edu>
2026-01-29 12:06:32 -0500

    Updated order of references in search_fallback.

---
+++ Main.py
@@ -0,0 +1,105 @@
+import os
+import ocrmypdf
+import pytesseract
+from pathlib import Path
+from PIL import Image
+
+# TODO add output folder path for network drive (use double forward slash [//] in file path)
+output_pdf = Path("")
+
+def convert_tiff(tiff):
+    if tiff.exists():
+        ocrmypdf.ocr(tiff, output_pdf, deskew=True, force_ocr=True)
+
+
+def search_folders(folder_path, search_for):
+    matched_files = []
+
+    # Supported image extensions
+    valid_ext = ('.pdf', '.jpg', '.jpeg', '.png', '.bmp', '.TIF', '.tiff', '.tif')
+
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(valid_ext):
+                img_path = os.path.join(root, file)
+
+                try:
+                    # Open image and convert to text
+                    text = pytesseract.image_to_string(Image.open(img_path))
+
+                    if search_for.lower() in text.lower():
+                        matched_files.append(img_path)
+
+                        print(f"Match found: {img_path}")
+
+                except Exception as e:
+                    print(f"Error processing {img_path}: {e}")
+
+    if len(matched_files) > 1:
+        date = input("Enter Date of Admission: ")
+
+        search_images(matched_files, date)
+
+    else:
+        if len(matched_files) == 0:
+            search_fallback(folder_path, search_for)
+
+        else:
+            convert_tiff(matched_files[0])
+
+
+def search_fallback(folder_path, search_for):
+    keywords = search_for.split()
+    partial_matched_files = []
+
+    valid_ext = ('.pdf', '.jpg', '.jpeg', '.png', '.bmp', '.TIF', '.tiff', '.tif')
+
+    for root, files in os.walk(folder_path):
+        for file in files:
+            if file.lower().endswith(valid_ext):
+                img_path = os.path.join(root, file)
+
+                try:
+                    # Open image and convert to text
+                    text = pytesseract.image_to_string(Image.open(img_path))
+                    matched_words = [word for word in keywords if word.lower() in text.lower()]
+
+                    # Threshold for "partial" match of name (50%)
+                    if len(matched_words) >= (len(keywords) / 2) and len(keywords) > 1:
+                        partial_matched_files.append(img_path)
+
+                    else:
+                        print(f"No matches found for {search_for}")
+
+                except Exception as e:
+                    print(f"Error processing {img_path}: {e}")
+
+
+def search_images(matched_files, search_for):
+    files = []
+    for file in matched_files:
+
+        try:
+            text = pytesseract.image_to_string(Image.open(file))
+
+            if search_for.lower() in text.lower():
+                files.append(file)
+
+                print(f"Match found: {file}")
+
+        except Exception as e:
+            print(f"Error processing {file}: {e}")
+
+    if len(files) == 1:
+        convert_tiff(files[0])
+
+    else:
+        if len(files) == 0:
+            print("No matching documents found.")
+
+
+if __name__ == "__main__":
+    # TODO add input folder path for network drive (use double forward slash [//] in file path)
+    input_tiff = Path()
+    name = input("Enter first and last name: ")
+    search_folders(input_tiff, name)
